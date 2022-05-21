027f01baf77d6939857685d480cbbba6f72383ff
Davit Ghonyan <dghonyan@c2r13s6.42yerevan.am>
2022-05-21 19:50:27 +0400

    starting init utils

--- mlx_hooks.c
+++ mlx_hooks.c
@@ -5,19 +5,23 @@
 
 static void	call_move(int keycode, t_mlx *mlx)
 {
-	if ((keycode == 13 || keycode == 126) && mlx->player->posy >= mlx->player->h)
+	if ((keycode == 13 || keycode == 126)
+		&& mlx->player->posy >= mlx->player->h)
 	{
 		move_up(mlx);
 	}
-	else if ((keycode == 1 || keycode == 125) && mlx->player->posy <= mlx->win_size_h - mlx->player->h)
+	else if ((keycode == 1 || keycode == 125)
+		&& mlx->player->posy <= mlx->win_size_h - mlx->player->h)
 	{
 		move_down(mlx);
 	}
-	else if ((keycode == 0 || keycode == 123) && mlx->player->posx >= mlx->player->w)
+	else if ((keycode == 0 || keycode == 123)
+		&& mlx->player->posx >= mlx->player->w)
 	{
 		move_left(mlx);
 	}
-	else if ((keycode == 2 || keycode == 124) && mlx->player->posx <= mlx->win_size_w - mlx->player->w)
+	else if ((keycode == 2 || keycode == 124)
+		&& mlx->player->posx <= mlx->win_size_w - mlx->player->w)
 	{
 		move_right(mlx);
 	}
@@ -25,15 +29,17 @@ static void	call_move(int keycode, t_mlx *mlx)
 
 int	key_hook(int keycode, t_mlx *mlx)
 {
+	char		*s;
+	char		*_s;
 	static int	count = 0;
-	char		*s = ft_itoa(count);
-	char		*_s = ft_strjoin("Moves: ", s);
-	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->imgs[0]->img, 0, 0);
-	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 0, 0, 0xFFFFFF, _s);
+
+	s = ft_itoa(count);
+	_s = ft_strjoin("Moves: ", s);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr,
+		mlx->imgs[0]->img, 0, 0);
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 0, 0, 0x3264a8, _s);
 	free(s);
 	free(_s);
-	// ft_printf(YELLOW "%d" COLOR_RESET, keycode);
-	//printf("JLKWKJW");
 	if (keycode == 53)
 	{
 		exit(EXIT_SUCCESS);
---
+++ mlx_init_utils.c
@@ -0,0 +1,13 @@
+#include "so_long.h"
+
+void	**allocate_floor(t_mlx *mlx)
+{
+	int		i;
+	int		j;
+	int		k;
+	void	**arr;
+
+	i = 0;
+	k = 0;
+	while 
+}
